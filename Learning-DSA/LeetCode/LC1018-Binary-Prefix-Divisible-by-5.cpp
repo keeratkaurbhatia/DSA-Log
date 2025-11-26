85298ce2b9a5a1fcf41440a9195f70f73d060dfd
keerat kaur <itskeeratkaur@gmail.com>
2025-11-26 21:14:22 +0530

    Create LC1018-Binary-Prefix-Divisible-by-5.cpp

---
+++ Learning-DSA/LeetCode/LC1018-Binary-Prefix-Divisible-by-5.cpp
@@ -0,0 +1,14 @@
+//O(n), O(n)
+
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> ans;
+        int rem = 0;  
+        for (int i = 0; i < nums.size(); i++) {
+            rem = (rem * 2 + nums[i]) % 5; 
+            ans.push_back(rem == 0);
+        }
+        return ans;
+    }
+};
