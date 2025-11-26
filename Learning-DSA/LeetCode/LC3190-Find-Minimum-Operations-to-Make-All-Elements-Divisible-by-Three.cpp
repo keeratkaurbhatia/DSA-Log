712feb9283fa96925146c253f5975698b3d94bb9
keerat kaur <itskeeratkaur@gmail.com>
2025-11-26 21:16:13 +0530

    Create LC3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.cpp

---
+++ Learning-DSA/LeetCode/LC3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.cpp
@@ -0,0 +1,13 @@
+//O(n),O(1)
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int count = 0;
+        for (int i : nums)
+        {
+            if (i%3!=0) count++;
+        }
+        return count;
+    }
+};
