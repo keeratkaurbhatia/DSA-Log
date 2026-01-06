05b51903c030832ed714f9b69e085062319336f9
keerat kaur <itskeeratkaur@gmail.com>
2026-01-06 20:54:33 +0530

    Implement solution for N-repeated element problem

---
+++ Learning-DSA/LeetCode/2026/Jan/LC961-N-Repeated-Element-in-Size-2N-Array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+       int n = nums.size();
+       if (nums[n/2]==nums[n/2-1] || nums[n/2]==nums[n/2+1]) return nums[n/2];
+       return nums[n/2-1]; 
+    }
+};
